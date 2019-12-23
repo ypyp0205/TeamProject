dc8e7f27066b17104440c0db4196bf712c85a91d
ypyp0205 <gogo5451@naver.com>
2019-12-23 22:20:35 +0900

    1

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -1,5 +1,9 @@
 package Controller;
 
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
+import java.util.List;
+
 import Dao.BookDao;
 import Dao.UserDao;
 import Service.DisPlayMenu;
@@ -9,6 +13,23 @@ import VO.UserVO;
 public class MainController {
 
     public static void main(String[] args) {
+    	
+    	BookDao dao = new BookDao();
+    	BookVO vo = new BookVO();
+    
+    	
+    	for (int i = 0; i < dao.BookList.size();  i++) {
+    		
+    		
+    		if(vo.getName().equals("자바의 정석")){
+			System.out.println("123");
+    		}
+		}
+    	
+    	
+    	
+    	
+    	
         DisPlayMenu disPlayMenu=new DisPlayMenu();
         disPlayMenu.defaultMenu();
         for (BookVO temp :BookDao.BookList) {
